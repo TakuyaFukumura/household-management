cfd7ff26d300dec7c19d5304939f0705772b07f6
Takuya-Fukumura <blackchoco1114@gmail.com>
2021-08-06 00:20:29 +0900

    組み込みh2dbの導入と利用

--- README.md
+++ README.md
@@ -4,4 +4,12 @@
 ## 実行可能 Jar の作成
 `$ ./mvnw clean package`
 ## Jar の実行
-`$ java -jar ./target/household-management-バージョン.jar`
+`$ java -jar ./target/*.jar`
+## 組み込みH2DB
+### H2コンソールの設定
+H2コンソールを利用する場合は`/src/main/resources/application.properties`に`spring.h2.console.enabled=true`を書き加えるか、下記コマンドを実行すること
+`$ ./mvnw spring-boot:run -Dspring-boot.run.arguments=--spring.h2.console.enabled=true`
+`$ java -jar ./target/*.jar --spring.h2.console.enabled=true`
+### H2コンソールの開き方
+ローカル環境では下記URLから開くことができる
+`http://localhost:8080/h2-console/`
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,16 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.boot.CommandLineRunner;
 
 @RestController
 @EnableAutoConfiguration
-public class Main {
+public class Main implements CommandLineRunner {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Value("${hello.world}") 
     private String helloWorld;
@@ -18,6 +24,15 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+    
+        //jdbcTemplate.execute("CREATE TABLE customers(" +
+        //        "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+    
+        //jdbcTemplate.update("INSERT INTO customers(first_name, last_name) VALUES ('John','Woo')");
     }
 
 }
