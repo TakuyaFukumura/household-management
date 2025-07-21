1f35b656dc87abc992b34d5b290302f909d58a11
takuya-fukumura <blackchoco1114@gmail.com>
2025-07-21 10:29:59 +0900

    不要記述削除

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,19 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
-//import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @SpringBootApplication
-public class Main implements CommandLineRunner {
-
-    @Autowired
-    JdbcTemplate jdbcTemplate;
+public class Main {
 
     @RequestMapping("/")
     String home() {
@@ -24,15 +17,4 @@ public class Main implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
-    // 起動時に自動実行されるメソッド
-    @Override
-    public void run(String... strings) throws Exception {
-    
-        //jdbcTemplate.execute("CREATE TABLE customers(" +
-        //        "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
-    
-        //jdbcTemplate.update("INSERT INTO customers(first_name, last_name) VALUES ('John','Woo')");
-    }
-
 }
